32d2bfaab536af8c5ebc16752dc05aca1df98313
RalphvanAsch <136352725+RalphvanAsch@users.noreply.github.com>
2023-06-12 16:57:54 +0200

    Update Hardware.py
    Code toegevoegd voor uitlezen van de voltage.

--- src/Hardware.py
+++ src/Hardware.py
@@ -16,3 +16,37 @@
 # Data wegschrijven naar ../test_data en NIET naar deze map of naar ../ (root)
 # dit ivm de gitignore/versie beheer
 
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+class ADCReader:
+    def __init__(self):
+        # Create the I2C bus
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        
+        # Create the ADC object using the I2C bus
+        self.ads = ADS.ADS1015(self.i2c)
+        
+        # Create single-ended input on channel 0
+        self.chan = AnalogIn(self.ads, ADS.P0)
+        
+        # Create differential input between channel 0 and 1
+        # self.chan = AnalogIn(self.ads, ADS.P0, ADS.P1)
+    
+    def read_adc(self):
+        print("{:>5}\t{:>5}".format('raw', 'v'))
+        while True:
+            print("{:>5}\t{:>5.3f}".format(self.chan.value, self.chan.voltage))
+            time.sleep(0.5)
+
+# Create an instance of the ADCReader class
+adc_reader = ADCReader()
+
+# Start reading the ADC values
+adc_reader.read_adc()
+
+#You can create an instance of the ADCReader class and call the read_adc() method to start reading the ADC values. 
+#The read_adc() method #will continuously print the raw ADC value and the corresponding voltage with a delay of 0.5 seconds between readings
